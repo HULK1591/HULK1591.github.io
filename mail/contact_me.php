7b4e91d15bb4bf172025ba7a27657217af7916ea
Shyam Patel <37850045+HULK1591@users.noreply.github.com>
2019-06-13 13:49:21 -0400

    Update email service
    Update email service in the contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "shyampatel1991@outlook.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Inquiry from Portfolio  Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: https://formspree.io/email@domain.tld\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: https://formspree.io/sampatel94@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
